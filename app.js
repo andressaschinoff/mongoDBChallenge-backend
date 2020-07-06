a7f18686cb9eb13f607d72727b06a85ce537276b
Andressa Schinoff <andressaschinoff@gmail.com>
2020-07-06 19:49:48 -0300

    change cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://grades-app-schinoff.herokuapp.com/',
   })
 );
 
